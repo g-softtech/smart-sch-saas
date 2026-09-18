edabec2aa326a1d381cf0992f4bd8c45322a797a
Gbemi'king <tosinjaiyeoba@yahoo.com>
2026-09-18 02:20:52 +0100

    chore: untrack pre-existing utility scripts

--- packages/core-platform/check-all-users.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const users = await prisma.user.findMany({
-    include: { memberships: { include: { role: true, tenant: true } } }
-  });
-  console.log(JSON.stringify(users, null, 2));
-}
-run().catch(console.error).finally(()=>prisma.$disconnect());
